f0034f0eff0e6c53e4784530cc6470eb39f09d7d
Huseyin Zengin <hzengin99@gmail.com>
2015-10-30 20:47:27 +0300

    Improved declerative trait using implicits and helpers

--- src/main/scala/hzengin/telegrambot/Declerative.scala
+++ src/main/scala/hzengin/telegrambot/Declerative.scala
@@ -8,51 +8,52 @@ trait Declerative {
   val updateHandler: UpdateHandler
   val telegramApi: TelegramApi
 
-  private def actionWrapper(action: (Message,User) => Unit) = {
-    update: Update =>
-      action(update.message, update.message.from.get)
+  private def actionWrapper(action: Message => Unit) = { update: Update =>
+    action(update.message)
   }
 
-  private def actionWrapper(action: Message => Unit) = {
-    update: Update =>
-      action(update.message)
+  private def conditionWrapper(condition: Message => Boolean) = { update: Update =>
+    condition(update.message)
   }
 
-  def on(condition: Message => Boolean)(action: Message => Unit) = {
-    updateHandler.registerCommand( (u: Update) => condition(u.message),actionWrapper(action))
+  class Condition(val condition: Message => Boolean) {
+    def perform(action: Message => Unit) = {
+      updateHandler.registerCommand(conditionWrapper(condition), actionWrapper(action))
+    }
   }
 
-  // Declerative way to add a simple text command that must be coming from a user
-  def on(command: String)(action: (Message, User) => Unit ) = {
-    updateHandler.registerCommand((u: Update) => u.message.text match {
-      case Some(text) if text == command && !u.message.from.isEmpty => true
-      case _ => false
-    }, actionWrapper(action))
-  }
+  def when(condition: Message => Boolean) = new Condition(condition)
 
-  def send(to: User)(text: String) = {
-    telegramApi.sendMessage(
-      SendMessageRequest(Right(to.id), text)
+  def on(command: String)(action: (Message) => Unit) = {
+    updateHandler.registerCommand(
+      conditionWrapper(
+        (message: Message) => {
+          message.text match {
+            case Some(text) if text == command => true
+            case _ => false
+          }
+        }
+      ),
+      actionWrapper(action)
     )
   }
 
-  def reply(message: Message)(text: String) = message.from match {
-    case Some(user) => message.chat match {
-      case Left(_) => telegramApi.sendMessage(
-        SendMessageRequest(
-          Right(user.id),
-          text,
-          replyTo = Some(message.id)
-        )
-      )
-      case Right(groupChat) => telegramApi.sendMessage(
-        SendMessageRequest(
-          Left(groupChat.id),
-          text,
-          replyTo = Some(message.id)
-        )
-      )
-    }
-    case _ =>
+  def send(text: String)(implicit message: Message) = {
+    telegramApi.sendMessage(
+      message.chat match {
+        case Left(user) => SendMessageRequest(Right(user.id), text)
+        case Right(groupChat) =>SendMessageRequest(Left(groupChat.id), text)
+      }
+    )
   }
+
+  def reply(text: String)(implicit message: Message) = {
+    telegramApi.sendMessage(
+      message.chat match {
+        case Left(user) => SendMessageRequest(Right(user.id), text, replyTo = Some(message.id))
+        case Right(groupChat) => SendMessageRequest(Left(groupChat.id), text, replyTo = Some(message.id))
+      }
+    )
+  }
+
 }
