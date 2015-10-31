eef2394edc7d58d8f3336a5e6a13c2334553a9a1
Huseyin Zengin <hzengin99@gmail.com>
2015-10-31 17:19:37 +0300

    added ssl support

--- src/main/scala/hzengin/telegrambot/TelegramBot.scala
+++ src/main/scala/hzengin/telegrambot/TelegramBot.scala
@@ -7,7 +7,8 @@ import akka.io.IO
 import hzengin.telegrambot.types.Update
 import hzengin.telegrambot.webhook._
 
-class TelegramBot(val token: String) {
+class TelegramBot {
+  val token: String = BotConfig.token
   val updateHandler: UpdateHandler = new UpdateHandler()
   implicit val system = ActorSystem("telegrambot-system")
   val telegramApi:TelegramApi = new TelegramApi(token, system)
--- src/main/scala/hzengin/telegrambot/Webhook.scala
+++ src/main/scala/hzengin/telegrambot/Webhook.scala
@@ -1,10 +1,49 @@
 package hzengin.telegrambot
+
+import hzengin.telegrambot.types.Update
+import hzengin.telegrambot.webhook._
 import akka.actor.{ ActorSystem, Props }
 import spray.can.Http
 import akka.io.IO
 
-import hzengin.telegrambot.types.Update
-import hzengin.telegrambot.webhook._
+import java.security.{SecureRandom, KeyStore}
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
+import spray.io._
+
+trait SecureWebhook {
+  implicit def sslContext: SSLContext = {
+    val keyStoreResource = BotConfig.WebhookConfig.keystore
+    val password = BotConfig.WebhookConfig.keystorePassword
+    val keyStore = KeyStore.getInstance("jks")
+    keyStore.load(getClass.getResourceAsStream(keyStoreResource), password.toCharArray)
+    val keyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+    keyManagerFactory.init(keyStore, password.toCharArray)
+    val trustManagerFactory = TrustManagerFactory.getInstance("SunX509")
+    trustManagerFactory.init(keyStore)
+    val context = SSLContext.getInstance("TLS")
+    context.init(keyManagerFactory.getKeyManagers, trustManagerFactory.getTrustManagers, new SecureRandom)
+    context
+  }
+
+  implicit def sslEngineProvider: ServerSSLEngineProvider = {
+    ServerSSLEngineProvider { engine =>
+      engine.setEnabledCipherSuites(Array("TLS_RSA_WITH_AES_256_CBC_SHA"))
+      engine.setEnabledProtocols(Array("SSLv3", "TLSv1"))
+      engine
+    }
+  }
+
+  def updateHandler: UpdateHandler
+  implicit val system: ActorSystem
+  val token: String
+  val serviceActor = system.actorOf(Props(new HookServiceActor(token, updateHandler)))
+
+  def run() = {
+    IO(Http) ! Http.Bind(serviceActor, interface = BotConfig.WebhookConfig.interface, port = BotConfig.WebhookConfig.port)
+  }
+
+}
+
 
 trait Webhook {
   def updateHandler: UpdateHandler
@@ -12,7 +51,7 @@ trait Webhook {
   val token: String
   val serviceActor = system.actorOf(Props(new HookServiceActor(token, updateHandler)))
 
-  def run(interface: String = "0.0.0.0", port: Int = 8080) = {
-    IO(Http) ! Http.Bind(serviceActor, interface = interface, port = port)
+  def run() = {
+    IO(Http) ! Http.Bind(serviceActor, interface = BotConfig.WebhookConfig.interface, port = BotConfig.WebhookConfig.port)
   }
 }
